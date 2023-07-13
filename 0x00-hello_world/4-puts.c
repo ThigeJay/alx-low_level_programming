952145d1667dbc715fbaf306385819681e342bc1
ThigeJay <thigejames25@gmail.com>
2023-07-13 02:38:45 -0700

    C files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main- always starts the programme.
+* Return: 0 on success
+*/
+
+int main() {
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
+
