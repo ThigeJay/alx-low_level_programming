5e4c2c983d88008de8d201a3b8ceb6b7d548919f
ThigeJay <thigejames25@gmail.com>
2023-07-13 02:45:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
 * Return: 0 on success
 */
 
-int main() {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+int main (main) 
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
 
