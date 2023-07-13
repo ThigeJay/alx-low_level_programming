e47545d0755281fb1002ccce3ea8823f4c9073cb
ThigeJay <thigejames25@gmail.com>
2023-07-13 04:08:47 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+* main - program starts
+* Return: 1 on success
+*/
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
