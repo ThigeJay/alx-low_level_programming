78e351561e7ff74e58362b6f4a27679544194617
ThigeJay <thigejames25@gmail.com>
2023-07-14 08:05:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,22 +10,19 @@
 */
 int main(void)
 {
-	int digit1;
-	int digit2;
+	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		for (digit2 = digit1; digit2 < 10; digit2++)
 		{
 			putchar(digit1 + '0');
-			putchar(',');
-			putchar('\t');
 			putchar(digit2 + '0');
 
-			if (!(digit1 == 8 && digit2 == 9))
+			if (!(digit1 == 9 && digit2 == 9))
 			{
 				putchar(',');
-				putchar('\t');
+				putchar(' ');
 			}
 		}
 	}
