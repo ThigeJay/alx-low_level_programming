6488fbd3b09ca809d47586dafb1238b7b6c58b7d
ThigeJay <thigejames25@gmail.com>
2023-08-01 05:13:20 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,30 +2,29 @@
 
 /**
  * _strspn - Gets the length of a prefix substring.
- * @s: The string to be scanned.
- * @accept: The string containing the list of characters to match in s.
+ * @s: string to be searched
+ * @accept: string containing the list of characters to match in s.
  *
- * Return: The number of bytes in s which consist only of bytes from accept.
+ * Return: bytes number in s
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int scan = 0;
+	unsigned int n = 0;
 	int index;
 
 	while (*s)
 	{
-		for (r = 0; accept[index]; index++)
+		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
 			{
-				scan++;
+				n++;
 				break;
 			}
-			else if (accept[index + 1] == '\0')
-				return (scan);
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
 		s++;
 	}
-	return (scan);
+	return (n);
 }
-
