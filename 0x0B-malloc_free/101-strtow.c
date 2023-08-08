010235ca1cd27a7f69110a07ea1ad988a6b51ff4
ThigeJay <thigejames25@gmail.com>
2023-08-08 13:57:36 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,6 +29,29 @@ int count_word(char *s)
 }
 
 /**
+ * allocate_memory - allocates memory and handles errors
+ * @matrix: double pointer to matrix
+ * @c: length of string
+ * @y: index
+ *
+ * Return: allocated memory
+ */
+char *allocate_memory(char **matrix, int c, int y)
+{
+	char *tmp = (char *) malloc(sizeof(char) * (c + 1));
+
+	if (tmp == NULL)
+	{
+		for (int x = 0; x < y; x++) /*Free previously allocated memory*/
+			free(matrix[x]);
+		free(matrix);
+		return (NULL);
+	}
+
+	return (tmp);
+}
+
+/**
  * **strtow - splits a string into words
  * @str: string to split
  *
@@ -57,14 +80,9 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = x;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				tmp = allocate_memory(matrix, c, y);
 				if (tmp == NULL)
-				{
-					for (x = 0; x < y; x++) /*Free previously allocated memory*/
-						free(matrix[x]);
-					free(matrix);
 					return (NULL);
-				}
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
