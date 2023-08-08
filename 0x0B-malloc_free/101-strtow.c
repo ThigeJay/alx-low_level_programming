96ed62045ba98408fde229d8f4dec607b394a22e
ThigeJay <thigejames25@gmail.com>
2023-08-08 13:51:55 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count_word - counts number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int pointer, c, word;
+
+	pointer = 0;
+	word = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			pointer = 0;
+		else if (pointer == 0)
+		{
+			pointer = 1;
+			word++;
+		}
+	}
+
+	return (word);
+}
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int x, y = 0, length = 0, words, c = 0, start, end;
+
+	while (*(str + length))
+		length++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (x = 0; x <= length; x++)
+	{
+		if (str[x] == ' ' || str[x] == '\0')
+		{
+			if (c)
+			{
+				end = x;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+				{
+					for (x = 0; x < y; x++) /*Free previously allocated memory*/
+						free(matrix[x]);
+					free(matrix);
+					return (NULL);
+				}
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[y] = tmp - c;
+				y++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = x;
+	}
+
+	matrix[y] = NULL;
+
+	return (matrix);
+}
