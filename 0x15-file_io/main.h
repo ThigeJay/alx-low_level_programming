3667cbf497982f01de5525271a1ec1dce63ba191
ThigeJay <thigejames25@gmail.com>
2023-09-26 07:31:36 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/* Function Prototypes for first 3 tasks*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+/* Function Prototypes for cp task */
+char *generate_buffer(char *file_name);
+void close_descriptor(int file_descriptor);
+int main(int arg_count, char *args[]);
+
+#endif /* MAIN_H */
