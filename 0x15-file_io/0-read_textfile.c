675bf8380265cbc090be2c46b0219e61795c82a2
Thige254 <thigetj@gmail.com>
2023-11-27 18:58:53 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,48 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints
- * it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints a
+ * specified number of characters.
  * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * @num_chars: The number of characters to read and print.
  *
- * Return: The actual number of letters it could read and print.
- *         0 if the file cannot be opened or read, or if filename is NULL.
- *         0 if write fails or does not write the expected amount of bytes.
+ * Return: The actual number of characters read and printed.
+ * Returns 0 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t num_chars)
 {
-	int fd;
-	ssize_t read_chars, write_chars;
-	char *buf;
+	int file_descriptor;
+	ssize_t num_read, num_written;
+	char *buffer;
 
-	if (filename == NULL)
+	/* Check if filename is NULL */
+	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	/* Open the file for reading */
+	file_descriptor = open(filename, O_RDONLY);
+
+	/* Check if open operation fails */
+	if (file_descriptor == -1)
 		return (0);
 
-	buf = malloc(letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * num_chars);
 
-	read_chars = read(fd, buf, letters);
-	if (read_chars == -1)
-	{
-		free(buf);
-		close(fd);
+	/* Check if memory allocation fails */
+	if (!buffer)
 		return (0);
-	}
 
-	write_chars = write(STDOUT_FILENO, buf, read_chars);
-	if (write_chars == -1 || write_chars != read_chars)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+	/* Read characters from the file into the buffer */
+	num_read = read(file_descriptor, buffer, num_chars);
 
-	free(buf);
-	close(fd);
-	return (read_chars);
+	/* Write the content of the buffer to standard output */
+	num_written = write(STDOUT_FILENO, buffer, num_read);
+
+	/* Close the file descriptor and free the allocated memory */
+	close(file_descriptor);
+	free(buffer);
+
+	/* Return the actual number of characters read and printed */
+	return (num_written);
 }
